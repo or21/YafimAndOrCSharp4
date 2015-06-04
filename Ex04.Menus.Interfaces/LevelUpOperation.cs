85d2f620992541725dce32adf87b45b3de85e1b7
yafim <ifim.vo@gmail.com>
2015-06-04 16:38:46 +0300

    123

---
+++ Ex04.Menus.Interfaces/LevelUpOperation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ex04.Menus.Interfaces
+{
+    public class LevelUpOperation : IRunOperation
+    {
+        public void RunOperation()
+        {
+        }
+    }
+}
